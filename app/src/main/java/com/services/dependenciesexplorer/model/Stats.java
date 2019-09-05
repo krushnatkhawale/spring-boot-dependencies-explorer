5de821e26f5394d5a01b2b05de370ecfb440879e
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-09-05 21:59:34 +0100

    Adding changes

---
+++ app/src/main/java/com/services/dependenciesexplorer/model/Stats.java
@@ -0,0 +1,12 @@
+package com.services.dependenciesexplorer.model;
+
+public class Stats {
+
+    private int libCount;
+    private int appCount;
+
+    public Stats(int libCount, int appCount) {
+        this.libCount = libCount;
+        this.appCount = appCount;
+    }
+}
